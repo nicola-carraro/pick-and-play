bc127196abfd873c2bbd654992956fb7d8202b83
Nicola Carraro <Carraro@ed1.afpa>
2020-06-16 10:19:15 +0200

    Added new migrations

---
+++ Migrations/20200616081632_Decimal.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PickAndPlay.Migrations
+{
+    public partial class Decimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrixLocation",
+                table: "Jeu",
+                type: "decimal(18, 2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 0)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrixAchat",
+                table: "Jeu",
+                type: "decimal(18, 2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 0)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Editeur",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Link = table.Column<string>(maxLength: 50, nullable: true),
+                    Nom = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Editeur", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Editeur");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrixLocation",
+                table: "Jeu",
+                type: "decimal(18, 0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrixAchat",
+                table: "Jeu",
+                type: "decimal(18, 0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)",
+                oldNullable: true);
+        }
+    }
+}
