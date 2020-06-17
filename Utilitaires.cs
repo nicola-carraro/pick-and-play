309faecb72ed36a67b894c1e266006917db235c8
Nicola Carraro <carraron985@gmail.com>
2020-06-17 15:56:01 +0200

    Added Utilitaires.AfficherAnnee

--- Utilitaires.cs
+++ Utilitaires.cs
@@ -9,11 +9,14 @@ namespace PickAndPlay
     public static class Utilitaires
     {
 
-        public static string Display<T>(Nullable<T> nullable, string defaultString) where T : struct => 
+        public static string Afficher<T>(Nullable<T> nullable, string defaultString) where T : struct => 
             nullable.HasValue ? nullable.Value.ToString() : defaultString;
 
 
-        public static string Display<T>(T reference, string defaultString) where T : class => 
+        public static string Afficher<T>(T reference, string defaultString) where T : class => 
             reference != null ? reference.ToString() : defaultString;
+
+        public static string AfficherAnnee(DateTime? date, string defaultString)  =>
+            date != null ? date.Value.Year.ToString() : defaultString;
     }
 }
